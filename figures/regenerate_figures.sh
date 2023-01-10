485cf5cb5b5314865219ec3fc5042338be17ddaf
Jon Clucas <jon.clucas@childmind.org>
2023-01-10 12:32:41 -0500

    :art: Improve resolution

--- figures/regenerate_figures.sh
+++ figures/regenerate_figures.sh
@@ -2,7 +2,7 @@ for FILE in `ls *.tex`
 do
   NAME=${FILE::${#FILE}-4}
   latex -output-format=pdf $FILE
-  convert -define png:color-type=6 -background white -alpha remove -alpha off $NAME.pdf $NAME.png
+  convert -density 600 -quality 100 -define png:color-type=6 -background white -alpha remove -alpha off $NAME.pdf $NAME.png
 done
 rm -rf *.aux *.log
 echo "Figures regenerated!"
